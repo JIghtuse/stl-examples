b54733f5e63eb5d7d0cac012dff7a55c33554ae1
Boris Egorov <egorov@linux.com>
2016-02-17 13:25:13 +0600

    find_if_not: Add example

---
+++ src/find_if_not.cxx
@@ -0,0 +1,27 @@
+#include <algorithm>
+#include <array>
+#include <iostream>
+
+int main()
+{
+    std::array<int, 6> values{ { 3, 4, 2, 1, -1, 0 } };
+
+    std::cout << "Array values: ";
+    for (auto i : values)
+        std::cout << i << " ";
+    std::cout << std::endl;
+
+    auto positive = [](auto i) { return i > 0; };
+
+    auto positive_pos = std::find_if(values.begin(), values.end(), positive);
+    if (positive_pos != values.end())
+        std::cout << "Index of first positive value: "
+                  << std::distance(values.begin(), positive_pos)
+                  << std::endl;
+
+    auto non_positive_pos = std::find_if_not(values.begin(), values.end(), positive);
+    if (non_positive_pos != values.end())
+        std::cout << "Index of first non-positive value: "
+                  << std::distance(values.begin(), non_positive_pos)
+                  << std::endl;
+}
