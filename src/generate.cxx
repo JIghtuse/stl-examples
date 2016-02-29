0119bccf474b165cc5f0f0f03a4a10fd9f2c286f
Boris Egorov <egorov@linux.com>
2016-02-29 23:34:24 +0600

    generate: Add example

---
+++ src/generate.cxx
@@ -0,0 +1,32 @@
+#include <algorithm>
+#include <array>
+#include <iostream>
+#include <tuple>
+
+struct FibGenerator {
+    FibGenerator()
+        : a{ 1 }
+        , b{ 0 }
+    {
+    }
+    size_t operator()()
+    {
+        std::tie(a, b) = std::pair<size_t, size_t>(b + a, a);
+        return b;
+    }
+
+private:
+    size_t a;
+    size_t b;
+};
+
+int main(void)
+{
+    auto values = std::array<size_t, 30>();
+    std::generate(values.begin(), values.end(), FibGenerator{});
+
+    std::cout << "values: ";
+    for (auto e : values)
+        std::cout << e << ' ';
+    std::cout << std::endl;
+}
