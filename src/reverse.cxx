6992392b8b2736b2db78cac3d09ac8ba4a6d9b5c
Boris Egorov <egorov@linux.com>
2016-03-21 20:09:42 +0600

    reverse: Add example

---
+++ src/reverse.cxx
@@ -0,0 +1,66 @@
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+void print_file(const std::string& fname, bool reversed)
+{
+    auto in{ std::ifstream{ fname } };
+
+    if (!in) {
+        std::cerr << "Cannot print file " << fname << '\n';
+        return;
+    }
+
+    auto lines{ std::vector<std::string>{} };
+    for (std::string line; std::getline(in, line);) {
+        lines.push_back(line);
+    }
+
+    if (reversed) {
+        std::reverse(lines.begin(), lines.end());
+    }
+
+    for (const auto& line : lines) {
+        std::cout << line << '\n';
+    }
+}
+
+// "cat"-like utility
+int main(int argc, char* argv[])
+{
+    const char* program_name = argv[0];
+
+    auto usage = [program_name](std::ostream& out = std::cerr) {
+        out << "usage: " << program_name << " [-r] filename.txt\n";
+    };
+
+    if (argc < 2) {
+        usage();
+        return 1;
+    }
+
+    auto reversed{ false };
+    std::string filename;
+
+    if (argc == 3) {
+        if (std::string{ argv[1] } == std::string{ "-r" }) {
+            reversed = true;
+            filename = argv[2];
+        }
+        else {
+            usage();
+            return 1;
+        }
+    }
+    else if (argc == 2) {
+        filename = argv[1];
+    }
+    else {
+        usage();
+        return 1;
+    }
+
+    print_file(filename, reversed);
+}
