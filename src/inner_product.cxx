36b98fcbf2f8fad36dfbea02dd9ee60fd201fabc
Boris Egorov <egorov@linux.com>
2016-04-02 20:07:19 +0600

    inner_product: Add example

---
+++ src/inner_product.cxx
@@ -0,0 +1,19 @@
+#include <print_container.h>
+#include <functional>
+#include <numeric>
+#include <vector>
+
+int main()
+{
+    auto a{std::vector<int>{5, 4, 0, 2, 3}};
+    auto b{std::vector<int>{1, 4, 2, 5, 3}};
+
+    auto product = std::inner_product(a.begin(), a.end(), b.begin(), 0);
+    print_container(a, "a: ");
+    print_container(b, "b: ");
+    std::cout << "Inner product of a and b: " << product << std::endl;
+
+    auto hamming_distance = std::inner_product(a.begin(), a.end(), b.begin(), 0,
+                                               std::plus<int>(), std::not_equal_to<int>());
+    std::cout << "Hamming distance between a and b: " << hamming_distance << std::endl;
+}
