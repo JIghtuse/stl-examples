9326fce9a2ae7adc748180ef379ea41a81bdc322
Boris Egorov <egorov@linux.com>
2016-06-04 23:43:54 +0600

    stats: std::minmax_element, std::accumulate examples

---
+++ src/stats.cxx
@@ -0,0 +1,44 @@
+#include <print_container.h>
+#include <cassert>
+#include <cmath>
+#include <algorithm>
+#include <numeric>
+#include <vector>
+
+double mean(const std::vector<int>& data)
+{
+    assert(!data.empty());
+    return std::accumulate(data.begin(), data.end(), 0.0) / data.size();
+}
+
+double variance(const std::vector<int>& data, double mean_value)
+{
+    auto sum = std::accumulate(data.begin(), data.end(), 0.0, [mean_value](double& acc, int x) {
+        return acc += (x - mean_value) * (x - mean_value);
+    });
+    return sum / data.size();
+}
+
+double standard_deviation(const std::vector<int>& data, double mean_value)
+{
+    return std::sqrt(variance(data, mean_value));
+}
+
+int main()
+{
+    auto numbers = std::vector<int>{
+         1,  2,  3,  4,  5,  6,  7,  8,  9,
+        -1, -2, -3, -4, -5, -6, -7, -8, -9,
+         0
+    };
+    print_container(numbers, "v: ");
+
+    auto minmax = std::minmax_element(numbers.begin(), numbers.end());
+    std::cout << "minimum element:    " << *minmax.first << std::endl;
+    std::cout << "maximum element:    " << *minmax.second << std::endl;
+
+    auto mean_value = mean(numbers);
+    std::cout << "mean value:         " << mean_value << std::endl;
+    std::cout << "variance:           " << variance(numbers, mean_value) << std::endl;
+    std::cout << "standard deviation: " << standard_deviation(numbers, mean_value) << std::endl;
+}
